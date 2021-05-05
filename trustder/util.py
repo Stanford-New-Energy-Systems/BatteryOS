c05ad9943573e9a4f9ebd6e73f3557b7cbd26a0c
Nicholas Mosier <nh.mosier@gmail.com>
2021-05-05 08:51:44 -0700

    add global configurable time

--- trustder/util.py
+++ trustder/util.py
@@ -4,3 +4,36 @@ def all_different(items) -> bool:
 
 def all_same(items) -> bool:
     return len(set(items)) == 1
+
+class Time:
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        raise NotImplementedError
+
+import time as systime
+class SystemTime:
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self):
+        return systime.time()
+
+class DummyTime:
+    def __init__(self, time):
+        super().__init__()
+        self._time = time 
+
+    def __call__(self):
+        return self._time
+
+    def set_time(self, time):
+        assert self._time <= time
+        self._time = time
+
+    def tick(self, duration):
+        assert duration >= 0
+        self._time += duration
+
+time = SystemTime()
