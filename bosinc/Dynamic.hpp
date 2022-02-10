ebfa4d2233ecfb5c8d00cfe9509c80d7a66855c7
Ouyancheng <1024842937@qq.com>
2022-02-10 11:10:42 -0800

    rename dll => dylib

--- bosinc/Dynamic.hpp
+++ bosinc/Dynamic.hpp
@@ -20,8 +20,8 @@ public:
         return 0;
     }
 private: 
-    static std::map<std::string, void*> loaded_dlls;
-    void *dll_handle;
+    static std::map<std::string, void*> loaded_dynamic_libs;
+    void *dynamic_lib_handle;
     init_func_t        init_func;
     destruct_func_t    destruct_func;
     get_status_func_t  get_status_func;
@@ -33,7 +33,7 @@ public:
     DynamicBattery(
         const std::string &name,
         const std::chrono::milliseconds &max_staleness_ms,
-        const std::string &dll_path, 
+        const std::string &dynamic_lib_path, 
         const std::string &init_func_name, 
         const std::string &destruct_func_name, 
         const std::string &get_status_func_name, 
@@ -49,38 +49,41 @@ public:
         initialized(false),
         init_result(0)
     {
-        if (loaded_dlls.find(dll_path) != loaded_dlls.end()) {
-            this->dll_handle = loaded_dlls[dll_path];
+        if (loaded_dynamic_libs.find(dynamic_lib_path) != loaded_dynamic_libs.end()) {
+            this->dynamic_lib_handle = loaded_dynamic_libs[dynamic_lib_path];
         } 
         else {
-            this->dll_handle = dlopen(dll_path.c_str(), RTLD_LAZY);
-            if (!this->dll_handle) {
-                WARNING() << "DLL not opened! Error: " << dlerror();
-                this->dll_handle = 0;
+            this->dynamic_lib_handle = dlopen(dynamic_lib_path.c_str(), RTLD_LAZY);
+            if (!this->dynamic_lib_handle) {
+                WARNING() << "dynamic_lib not opened! Error: " << dlerror();
+                this->dynamic_lib_handle = 0;
                 return;
             } 
         }
         bool failed = false;
-        this->init_func = (init_func_t)dlsym(this->dll_handle, init_func_name.c_str());
+        this->init_func = (init_func_t)dlsym(this->dynamic_lib_handle, init_func_name.c_str());
         if (!init_func) { WARNING() << "init_func not found! init_func = " << init_func_name << " Error: " << dlerror(); failed = true; }
-        this->destruct_func = (destruct_func_t)dlsym(this->dll_handle, destruct_func_name.c_str());
+        this->destruct_func = (destruct_func_t)dlsym(this->dynamic_lib_handle, destruct_func_name.c_str());
         if (!destruct_func) { WARNING() << "destruct_func not found! destruct_func = " << destruct_func_name << " Error: " << dlerror(); failed = true; }
-        this->get_status_func = (get_status_func_t)dlsym(this->dll_handle, get_status_func_name.c_str());
+        this->get_status_func = (get_status_func_t)dlsym(this->dynamic_lib_handle, get_status_func_name.c_str());
         if (!get_status_func) { WARNING() << "get_status_func not found! get_status_func = " << get_status_func_name << " Error: " << dlerror(); failed = true; }
-        this->set_current_func = (set_current_func_t)dlsym(this->dll_handle, set_current_func_name.c_str());
+        this->set_current_func = (set_current_func_t)dlsym(this->dynamic_lib_handle, set_current_func_name.c_str());
         if (!set_current_func) { WARNING() << "set_current_func not found! set_current_func = " << set_current_func_name << " Error: " << dlerror(); failed = true; }
-        this->get_delay_func = (get_delay_func_t)dlsym(this->dll_handle, get_delay_func_name.c_str());
+        this->get_delay_func = (get_delay_func_t)dlsym(this->dynamic_lib_handle, get_delay_func_name.c_str());
         if (!get_delay_func) {
             this->get_delay_func = &no_delay;
         }
 
         if (failed) {
-            WARNING() << "DLL failed to find required symbols!";
-            init_func = destruct_func = get_status_func = set_current_func = nullptr;
+            WARNING() << "dynamic_lib failed to find required symbols!";
+            init_func = nullptr;
+            destruct_func = nullptr;
+            get_status_func = nullptr;
+            set_current_func = nullptr;
             return; 
         } 
         
-        loaded_dlls[dll_path] = this->dll_handle;
+        loaded_dynamic_libs[dynamic_lib_path] = this->dynamic_lib_handle;
         this->initialized = true;
 
         init_result = this->init_func(init_argument);
