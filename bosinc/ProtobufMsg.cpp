1fcaa6d12352fc2591e48aac0f44d6c299f2b7b9
Ouyancheng <1024842937@qq.com>
2022-02-25 22:27:14 -0800

    change protobuf namespace

--- bosinc/ProtobufMsg.cpp
+++ bosinc/ProtobufMsg.cpp
@@ -3,7 +3,7 @@
 
 int test_protobuf() {
     GOOGLE_PROTOBUF_VERIFY_VERSION; 
-    bos::BatteryStatus battery_status_msg; 
+    bosproto::BatteryStatus battery_status_msg; 
     LOG() << "battery_status_msg.IsInitialized() = " << battery_status_msg.IsInitialized(); 
     return 0; 
 }
