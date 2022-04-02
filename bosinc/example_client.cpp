6f9b097b2c53a749e1da1482513a906c96334cce
Ouyancheng <1024842937@qq.com>
2022-04-02 15:49:54 -0700

    make sonnen a plugin

--- bosinc/example_client.cpp
+++ bosinc/example_client.cpp
@@ -116,13 +116,13 @@ void make_pseudo() {
 
 void make_dynamic() {
     // make_dynamic(
-    //     "dyn", "../example/build/libdynamicnull.dylib", 1000, 
+    //     "dyn", "../plugins/build/libdynamicnull.dylib", 1000, 
     //     (void*)a, "init", "destroy", "get_status", "set_current"); 
     bosproto::AdminMsg msg; 
     bosproto::MakeDynamic *args = msg.mutable_make_dynamic(); 
     args->set_name("dyn"); 
     args->set_max_staleness_ms(123); 
-    args->set_dynamic_lib_path("../example/build/libdynamicnull" DLL_SUFFIX); 
+    args->set_dynamic_lib_path("../plugins/build/libdynamicnull" DLL_SUFFIX); 
     std::string *init_arg = args->mutable_init_argument(); 
     (*init_arg) = "aaa"; 
     args->set_init_func_name("init"); 
@@ -145,8 +145,63 @@ void make_dynamic() {
     close(ofd); 
     return; 
 }
+struct sonnen_init_args {
+    int64_t serial; 
+    int64_t max_capacity_Wh; 
+    int64_t max_discharging_current_mA; 
+    int64_t max_charging_current_mA; 
+    int64_t terminate; 
+};
+void make_sonnen() {
+    // make_dynamic(
+    //     "sonnen", "../plugins/build/libsonnen.dylib", 1000, 
+    //     (void*)a, "init", "destroy", "get_status", "set_current"); 
+    bosproto::AdminMsg msg; 
+    bosproto::MakeDynamic *args = msg.mutable_make_dynamic(); 
+    args->set_name("sonnen"); 
+    args->set_max_staleness_ms(1000); 
+    args->set_dynamic_lib_path("../plugins/build/libsonnen" DLL_SUFFIX); 
+
+    sonnen_init_args iargs;
+    iargs.serial = atoll(std::getenv("SONNEN_SERIAL1")); 
+    iargs.max_capacity_Wh = 10000;
+    iargs.max_charging_current_mA = 30000; 
+    iargs.max_discharging_current_mA = 30000; 
+    iargs.terminate = 0; 
+    std::string *init_arg = args->mutable_init_argument(); 
+    init_arg->reserve(sizeof(sonnen_init_args)); 
+    char *bptr = (char*)&iargs;
+    for (size_t i = 0; i < sizeof(sonnen_init_args); ++i) {
+        init_arg->push_back(bptr[i]); 
+    }
+    args->set_init_func_name("init"); 
+    args->set_destruct_func_name("destroy"); 
+    args->set_get_status_func_name("get_status"); 
+    args->set_set_current_func_name("set_current"); 
+    args->set_get_delay_func_name("get_delay"); 
+
+    int ifd = open(BOSDIR "/admin_input", O_WRONLY); 
+    if (ifd < 0) {
+        std::cerr << "failed to open? errno = " << errno << std::endl; 
+        return; 
+    }
+    msg.SerializeToFileDescriptor(ifd); 
+    close(ifd); 
+    int ofd = open(BOSDIR "/admin_output", O_RDONLY); 
+    bosproto::AdminResp aresp; 
+    std::cout << "parsing" << std::endl; 
+    aresp.ParseFromFileDescriptor(ofd); 
+    std::cout << "parsed" << std::endl; 
+    close(ofd); 
+    return; 
+}
+
 
 int main() {
+    make_sonnen(); 
+    sleep(3); 
+    get_status("sonnen"); 
+#if 0
     get_status("nullbat"); 
     std::cout << "------------------------------------" << std::endl;
     sleep(3); 
@@ -164,6 +219,7 @@ int main() {
     sleep(3); 
     ask_shutdown(); 
     std::cout << "------------------------------------" << std::endl;
+#endif 
     return 0; 
 }
 
